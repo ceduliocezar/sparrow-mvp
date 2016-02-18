22222444ae684473bfebf5685e441a2206792abb
Cedulio Cezar <ceduliocezar@gmail.com>
2016-02-17 23:58:47 -0200

    tests adjusts

--- data/src/test/java/com/cedulio/sparrow/data/repository/BillDataRepositoryTest.java
+++ data/src/test/java/com/cedulio/sparrow/data/repository/BillDataRepositoryTest.java
@@ -13,6 +13,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
+import static org.mockito.Mockito.when;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class BillDataRepositoryTest {
@@ -26,6 +28,9 @@ public class BillDataRepositoryTest {
     @Test
     public void testCloud() throws Exception {
 
+
+        mockApplicationContext();
+
         BillDataRepository repository = new BillDataRepository(context);
 
         List<Bill> list = repository.getBills();
@@ -33,14 +38,33 @@ public class BillDataRepositoryTest {
         Assert.assertFalse("The list should not be empty", list.isEmpty());
     }
 
+    private void mockApplicationContext() {
+        when(context.getApplicationContext()).thenReturn(context);
+    }
+
     @Test
     public void testCloudMapper() throws Exception {
+
+        mockApplicationContext();
 
         BillDataRepository repository = new BillDataRepository(context);
 
         List<Bill> list = repository.getBills();
 
+
+
         Assert.assertNotNull("Not mapped correctly id null", list.get(0).getId());
+    }
+
+    @Test
+    public void testGetBills() throws Exception {
+
+        mockApplicationContext();
+        BillDataRepository repository = new BillDataRepository(context);
+
+        List<Bill>  bills =   repository.getBills();
+
+        Assert.assertFalse("Bill list should not be empty", bills.isEmpty());
     }
 
 }
